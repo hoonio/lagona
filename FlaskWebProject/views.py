dee3dd9f75cb2096a77624ddcd58e1da9537cbc9
Hoonio <hoonio@hoonio.com>
2016-03-19 23:16:26 +0000

    remove sqllite dependency

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -1,15 +1,15 @@
 from os import environ
 from flask import Flask, render_template, request, redirect, jsonify, url_for
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from database_setup import Base, Restaurant, MenuItem
+# from sqlalchemy import create_engine
+# from sqlalchemy.orm import sessionmaker
+# from database_setup import Base, Restaurant, MenuItem
 from FlaskWebProject import app
 
-engine = create_engine('sqlite:///restaurantmenu.db')
-Base.metadata.bind = engine
-
-DBSession = sessionmaker(bind=engine)
-session = DBSession()
+# engine = create_engine('sqlite:///restaurantmenu.db')
+# Base.metadata.bind = engine
+#
+# DBSession = sessionmaker(bind=engine)
+# session = DBSession()
 
 @app.route('/restaurant/<int:restaurant_id>/menu/JSON')
 def restaurantMenuJSON(restaurant_id):
@@ -35,7 +35,8 @@ def restaurantsJSON():
 @app.route('/')
 @app.route('/restaurant/')
 def showRestaurants():
-    restaurants = session.query(Restaurant).all()
+    restaurants = [{'name': 'The CRUDdy Crab', 'id': '1'}, {'name':'Blue Burgers', 'id':'2'},{'name':'Taco Hut', 'id':'3'}]
+    # restaurants = session.query(Restaurant).all()
     # return "This page will show all my restaurants"
     return render_template('restaurants.html', restaurants=restaurants)
 
