6fa539ceb4a07e4151aea89f409b130754906d45
Hoonio <hoonio@hoonio.com>
2016-04-04 13:09:30 +0100

    resolve project name

--- runserver.py
+++ runserver.py
@@ -3,7 +3,7 @@ This script runs the Lagona application using a development server.
 """
 
 from os import environ
-from app import app
+from Lagona  import app
 
 if __name__ == '__main__':
     HOST = environ.get('SERVER_HOST', 'localhost')
